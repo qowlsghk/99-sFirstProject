c815a2fbfc59dc2f378dc8f948c208c00b477b66
bjh <wlsghk0521@gmail.com>
2022-12-20 16:47:08 +0900

    파일명 변경

--- js/detail.js
+++ js/detail.js
@@ -287,5 +287,5 @@ $topBtn.addEventListener('click', (e) => {
 
 const $web = document.querySelector('.copy__btn1');
 $web.addEventListener('click', (e) => {
-  window.location.href = 'index.html';
+  window.location.href = '../html/index.html';
 });
